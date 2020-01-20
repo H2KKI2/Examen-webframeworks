fec35ff71cba7d2882d3d2a48aa3af0b66f2736c
H2KKI2 <36642902+H2KKI2@users.noreply.github.com>
2020-01-20 11:48:13 +0100

    Examenvraag2 student exists
    Als er een student exist, en je wilt deze toevoegen zal er een foutmelding komen te staan en zal deze niet worden toegevoegd aan de database.

--- Express-Examen/app/routes/students.js
+++ Express-Examen/app/routes/students.js
@@ -5,15 +5,15 @@ var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var db;
 MongoClient.connect('mongodb://localhost:27017', (err, database) => {
-    if(err) return console.log(err);
+    if (err) return console.log(err);
     db = database.db('exam');
 })
 
 // studenten tonen
 router.get('/', (req, res) => {
     db.collection('students').find().toArray((err, result) => {
-        if(err) return console.log(err);
-        res.render('list.ejs', {students: result});
+        if (err) return console.log(err);
+        res.render('list.ejs', { students: result });
     })
 })
 
@@ -22,8 +22,17 @@ router.get('/add/', (req, res) => {
     res.render('add.ejs');
 })
 
-router.post('/add',(req, res) =>{
-    db.collection('students').insertOne(req.body);
-    res.redirect('/students');
+router.post('/add', (req, res) => {
+    db.collection('students').findOne({ naam: req.body.naam }, (err, result) => {
+        if (err) return console.log(err);
+        if (result != null) {
+            res.render('add_exists.ejs');
+        }
+        else {
+            db.collection('students').insertOne(req.body);
+            res.redirect('/students');
+        }
+
+    })
 })
 module.exports = router;
