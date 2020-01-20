d6b3e3328e069b76a9d08be3ad7a9ccfc448664b
H2KKI2 <36642902+H2KKI2@users.noreply.github.com>
2020-01-20 11:03:04 +0100

    Examenvraag1 onnodige code verwijderen
    Ik had al gecommit en er stond nog onnodige code in het bestand. Deze heb ik verwijderd.

--- Angular-Examen/src/app/service/product.service.ts
+++ Angular-Examen/src/app/service/product.service.ts
@@ -1,16 +1,12 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Product } from './product';
 
 @Injectable()
 export class ProductService {
 
-   private productsServiceURI: string = 'http://localhost:3000/products';
-   private contentHeaders: HttpHeaders;
 
-   constructor(private http: HttpClient) {
-      this.contentHeaders = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+   constructor() {
+      
    }
 
    // Get all products
